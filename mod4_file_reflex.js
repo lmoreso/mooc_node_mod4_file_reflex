33b4aea6b11d7864ef559c10bef632ef5010cdf4
Lluis Moreso Bosch <lmoreso@yahoo.com>
2019-03-09 13:00:54 +0100

    Exercici acabat

--- mod4_file_reflex.js
+++ mod4_file_reflex.js
@@ -1,18 +1,26 @@
+/* eslint-disable no-console */
+'use strict'
 
-    // Imports user module mod4-quote_of_the_day.js
-let my_mod = require("./mod4-quote_of_the_day");
+// Imports user module mod4-quote_of_the_day.js
+let my_mod = require("./mod4_quote_of_the_day");
 
-let fs = require("fs");   // Imports file system module
-
-console.log();
+console.log("");
 my_mod.quote_of_the_day();
 
-let delay = ((Math.ceil(Math.random()*5))*1000).toFixed(0);
+let delay = ((Math.ceil(Math.random() * 5)) * 1000).toFixed(0);
 setTimeout(reflexes, delay);
 
 
 function reflexes() {
-
     // ... include code here
-};
+    console.log("\nPress return:");
+    let start = new Date().getTime();
+    process.stdin.on('data', function() {
+        let time = new Date().getTime() - start;
+        console.log("Your time is: " + time + "ms");
+        process.exit();
+      })
+
+}
+
 
--- mod4_quote_of_the_day.js
+++ mod4_quote_of_the_day.js
@@ -1,2 +1,68 @@
+/* eslint-disable no-console */
+var fs = require('fs');
 
+function quote_of_the_day() {
+    let _fileToRead = 'mod4_quotes_of_the_day.json';
+    let _citas = [];
+    fs.readFile(
+        _fileToRead,
+        'utf8',
+        function (err, data) {
+            if (data) {
+                //console.log(data);
+                _citas = JSON.parse(data);
+                if (_citas.length > 0) {
+                    // console.log(`Se han encontrado ${_numCitas} citas:`);
+                    // _citas.forEach((miCita, indice) => console.log(indice, miCita));
+                    console.log("quote_of_the_day:");
+                    let indice = (Math.floor(Math.random() *_citas.length)).toFixed(0);
+                    console.log(_citas[indice]);                    
+                } else {
+                    console.log( `Se ha leido el fichero ${_fileToRead}, pero no se han encontrado citas.`);
+                }
+            } else {
+                console.log(`Se ha producido un error leyendo el fichero ${_fileToRead}: ${err}`);
+                //throw err;
+            }
+        }
+    );
+} 
 
+/* 
+function quote_of_the_day() {
+    let _fileToRead = './tests/mod4_quotes_of_the_day.json';
+    let _citas = [];
+    let _citascargadas = false;
+    let _numCitas = 0;
+    fs.readFile(_fileToRead,
+        'ascii',
+        function (err, data) {
+            if (data) {
+                //console.log(data);
+                _citas = JSON.parse(data);
+                if (_citas.length > 0) {
+                    _citascargadas = true;
+                    _numCitas = _citas.length;
+                    // console.log(`Se han encontrado ${_numCitas} citas:`);
+                    // _citas.forEach((miCita, indice) => console.log(indice, miCita));
+                } else {
+                    throw `Se ha leido el fichero ${_fileToRead}, pero no se han encontrado citas.`;
+                }
+            } else {
+                throw `Se ha producido un error leyendo el fichero ${_fileToRead}: ${err}`;
+                //throw err;
+            }
+        }
+    );
+
+    return (function () {
+        if (_citascargadas) {
+            let indice = (Math.floor(Math.random() *_numCitas)).toFixed(0);
+            return _citas[indice];
+        }
+    });
+
+} 
+*/
+
+exports.quote_of_the_day = quote_of_the_day;
